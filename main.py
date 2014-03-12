21a1142626aa762c63856046eb23b0cf1a25228d
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-12 16:02:53 -0400

    After week 3

--- main.py
+++ main.py
@@ -53,23 +53,50 @@ def get_path(v, ang):
 
     return np.array(arr)
 
-def show_paths(np_arrs):
-    for x in np_arrs:
-        plt.plot(x[:,0], x[:,1], 'b.-')
-    
-    plt.show()
+class Project(object):
 
+    def __init__(self):
+        self.GRAVITY = -9.8
+
+        self.BASEBALL_M = 0.145 # Mass (kg)
+        self.BASEBALL_CD = 0.2 # Drag coeff 
+        self.BASEBALL_P = 1.2 # Air denity @ sea level (kg/m^3)
+        self.BASEBALL_RAD = 0.076/2 # Radius (m)
+        self.BASEBALL_A = math.pi * self.BASEBALL_RAD ** 2 # Cross-sectional area (m^2)
+
+        # Air resistance
+        self.BASEBALL_B = 1/2 * self.BASEBALL_P * self.BASEBALL_CD * self.BASEBALL_A
+
+    def get_path(self, v, ang):
+        """Show the path of the baseball"""
+        ang = math.radians(ang)
+        arr = []
+        for t in range(-1000, 1000):
+            ms = t/100
+            vt = (self.BASEBALL_M * self.GRAVITY)/self.BASEBALL_B
+
+            x_pos = ((v * self.BASEBALL_M)/self.BASEBALL_B) * math.cos(ang) * (1 - math.exp((-self.BASEBALL_B * ms)/self.BASEBALL_M))
+            #y_pos = (v * self.BASEBALL_M/self.BASEBALL_B) * math.sin(ang) * (1 - math.exp((-self.BASEBALL_B * t)/(self.BASEBALL_M))) - vt * t
+            #y_pos = ((v * vt)/self.GRAVITY) * math.sin(ang) * (1 - math.exp((-self.GRAVITY * t)/(vt))) - (vt * t)
+            y_pos = -(self.BASEBALL_M * self.GRAVITY/self.BASEBALL_B) * ms + (self.BASEBALL_M / self.BASEBALL_B) * (v * math.sin(ang) + (self.BASEBALL_M * self.GRAVITY)/self.BASEBALL_B) * (1 - math.exp((-self.BASEBALL_B * ms)/self.BASEBALL_M))
+            arr.append((x_pos, y_pos))
+            #if (y_pos < 0):
+            #    break
+
+        return np.array(arr)
+
+
+    def show_paths(self, np_arrs):
+        for x in np_arrs:
+            plt.plot(x[:,0], x[:,1], 'b.-')
+    
+        plt.show()
+
+    def main(self):
+        
+        #self.show_paths((self.get_path(10, x) for x in range(1, 90, 10)))
+        self.show_paths((self.get_path(10, 45),))
 
 if __name__ == "__main__":
-    BASEBALL_M = 0.145 # Mass (kg)
-    BASEBALL_CD = 0.2 # Drag coeff 
-    BASEBALL_P = 1.2 # Air denity @ sea level (kg/m^3)
-    BASEBALL_RAD = 0.076/2 # Radius (m)
-    BASEBALL_A = math.pi * math.pow(BASEBALL_RAD, 2) # Cross-sectional area (m^2)
-
-    # Air resistance
-    BASEBALL_B = 1/2 * BASEBALL_P * BASEBALL_CD * BASEBALL_A
-    
-    show_paths((get_path(10, x) for x in range(1, 90, 10)))
-
+    Project().main()
 

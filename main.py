6f65e088da7d7db81b18bbe78ef06b7df3d3a005
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-26 16:10:27 -0400

    Got things working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from pylab import *
 from scipy.integrate import odeint
+from scipy.integrate import ode
 import math
 
 class Project(object):
@@ -18,12 +19,12 @@ class Project(object):
         # Air resistance
         self.B = (1/2) * self.P * self.CD * self.A
 
-    def state_function(self, s, dt):
+    def state_function(self, dt, s):
         """Compute and return the next state based on the passed in state and dt"""
-        vx = s[2] * dt
-        vy = s[3] * dt
-        ax = -self.B/self.M * math.sqrt(vx**2 + vy**2) * vx * dt
-        ay = -self.GRAVITY - self.B/self.M * math.sqrt(vx**2 + vy**2) * vy * dt
+        vx = s[2]
+        vy = s[3]
+        ax = -self.B * math.sqrt(vx**2 + vy**2) * vx
+        ay = -self.GRAVITY - self.B * math.sqrt(vx**2 + vy**2) * vy
         return [vx, vy, ax, ay]
 
     def get_path(self, v, ang):
@@ -33,12 +34,66 @@ class Project(object):
         # Initial state of the system (x_pos, y_pos, x_vel, y_vel)
         state = [0, 0, v * math.cos(ang), v * math.sin(ang)]
 
-        # Make a list of times (start at 0, step by 0.05, end at 3 sec)
-        # arange instead of range so floating points can be used
-        t = arange(0, 3, 0.05)
+        # dopri5 is equivalent to ode45 in MATLAB
+        solver = ode(self.state_function).set_integrator('dopri5')
+        solver.set_initial_value(state, 0)
 
-        # Return the data from the integral
-        return odeint(self.state_function, state, t)
+        sol = []
+
+        # Integrate while the ball is above the ground
+        while solver.y[1] >= 0:
+            solver.integrate(solver.t+0.1)
+            sol.append([solver.y[0], solver.y[1]])
+
+        # Plot the data
+        return np.array(sol)
+
+    def find_zero_intercept(self, p1, p2):
+        """On the line from p1 to p2, find where it intersects with 0"""
+        x1, y1 = p1
+        x2, y2 = p2
+
+        y3, y4 = 0, 0
+        x3 = x1
+        x4 = x2
+
+        epsilon = 1e-5
+
+        dist = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+        # If dist is zero, there is no intersection
+        if (dist == 0):
+            return None
+
+        # Get the x and y
+        pre = (x1 * y2 - y1 * x2)
+        post = (x3*y4 - y3*x4)
+        x = (pre * (x3 - x4) - (x1 - x2) * post) / dist
+        y = (pre * (y3 - y4) - (y1 - y2) * post) / dist
+
+        # Check if the x and y coordinates are within both lines
+        if (x < min(x1, x2) - epsilon or x > max(x1, x2) + epsilon or x < min(x3, x4) - epsilon or x > max(x3, x4) + epsilon):
+            return None
+        if (y < min(y1, y2) - epsilon or y > max(y1, y2) + epsilon or y < min(y3, y4) - epsilon or y > max(y3, y4) + epsilon):
+            return None
+
+        # Return the point of intersection
+        return (x, y)
+
+    def get_best_path(self):
+        """Iterate over all angles and find the path with the max distance"""
+        curr_max = 0
+        curr_angle = 0
+        for angle in frange(40, 50, 0.01):
+            path = self.get_path(50, angle)
+            intercept = self.find_zero_intercept(path[-1], path[-2])[0]
+            if intercept > curr_max:
+                curr_max = intercept
+                curr_angle = angle
+            else:
+                break
+
+        return curr_angle, curr_max
 
     def show_paths(self, np_arrs):
         """Draw the passed in plots"""
@@ -53,17 +108,19 @@ class Project(object):
 
         handles, legends = subplot.get_legend_handles_labels()
 
-        plt.legend(handles, legends)
+        #plt.legend(handles, legends)
 
         plt.title('Path of Baseball')
         plt.xlabel('x (m)')
         plt.ylabel('y (m)')
+
         plt.show()
 
     def main(self):
         """Run the simulations"""
-
-        self.show_paths((self.get_path(10, x) for x in range(1, 90, 10)))
+        best = self.get_best_path()
+        print ("Maximum distance {1:.2f} obtained by launching at angle {0:.2f}".format(*best))
+        self.show_paths((self.get_path(50, best[0]),))
 
 if __name__ == "__main__":
     Project().main()
